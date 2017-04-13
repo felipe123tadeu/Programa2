745197a85986e12bfd2514d56c8734071d7ba3e8
Felipe <seu@email.com>
2017-04-13 17:55:08 -0300

    first commit

---
+++ 40.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+ main(){
+	int num;
+	printf("digite um numero\n");
+	scanf("%d", &num);
+	if(num>20)
+	printf("O numero digitado foi %d\n", num);
+	system ("pause");
+}
